568c03fe5222cdb58ebd2dce2672be6ff0b3ed74
Andrew McGregor <andy@txm.net>
2018-02-19 15:54:35 +0000

    eslint Ember.$

--- app/routes/feed.js
+++ app/routes/feed.js
@@ -1,7 +1,8 @@
 import Route from '@ember/routing/route';
+import $ from 'jquery';
 
 export default Route.extend({
   model() {
-    return Ember.$.getJSON("http://localhost:8080/api/activity-feeds");
+    return $.getJSON("http://localhost:8080/api/activity-feeds");
   }
 });
