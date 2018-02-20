dc84509b4998d7149964cd6e4dad59c7c3e1a2aa
Andrew McGregor <andy@txm.net>
2018-02-20 13:10:57 +0000

    stars

--- app/helpers/buttons.js
+++ app/helpers/buttons.js
@@ -9,14 +9,21 @@ export function buttons(params, { category, alertType }) {
     if(category === 'VISIT') {
       btnClass = 'btn-primary';
       btnText = 'Change booking'
+
+      return (
+        `<a class="btn btn-aem ${btnClass} mt2 pull-right text-bold" role="button" href="#">
+          ${btnText}
+        </a>`
+      );
     } 
+
     return (
       `<a class="btn btn-aem ${btnClass} mt2 pull-right text-bold" role="button" href="#">
-        ${btnText}
+        <i class="fa fa-angle-right"></i>${btnText}
       </a>`
     );
-
   }
+
   if(alertType === 'ACTIONNEEDED') {
     let btnPrimaryText = '';
     let btnSecondaryText = '';
@@ -32,17 +39,38 @@ export function buttons(params, { category, alertType }) {
 
     return (
       `<a class="btn btn-aem btn-primary mt2 pull-right" role="button" href="#">
-        <i class="fa fa-angle-right"></i>${btnPrimaryText}
+        ${btnPrimaryText}
       </a>
       <a class="btn btn-aem btn-tertiary mt2 pull-left" role="button" href="#">
         <i class="fa fa-angle-right"></i>${btnSecondaryText}
       </a>`
     );
   }
-  if(alertType === 'COMPLETE') {
-    return 'stars here';
-  }
 
+  if(alertType === 'COMPLETE') {
+    return `
+<!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.checked {
+  color: #09f;
+}
+.center {
+  text-align: center;
+  padding-top: 10px;
+  font-size: 1.5em;
+  color: white;
+}
+</style>
+<div class="center">
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star "></span>
+<span class="fa fa-star "></span>
+</div>
+    `;
+  }
 
   return '';
 }
