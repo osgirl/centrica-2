198077bfa67376178b0ae5d78b00bbeb31fb4aba
Andrew McGregor <andy@txm.net>
2018-02-20 11:22:13 +0000

    styling

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,12 +1,6 @@
 import { helper } from '@ember/component/helper';
 import moment from 'moment';
 
-export function formatDate(epoch) {
-  epoch = 1519060249;
-  return moment(epoch).format('MMMM Do YYYY, h:mm:ss a');
-  const d = new Date(epoch * 1000);
-  const months = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-  return `${d.getHours()}:${d.getMinutes()}, ${d.getDay()} ${months[d.getMonth()]} ${d.getFullYear()}`;
-}
+export const formatDate = (epoch) => moment(epoch).format('h:mma, ddd Do MMMM YYYY');
 
 export default helper(formatDate);
--- app/routes/feed.js
+++ app/routes/feed.js
@@ -3,6 +3,6 @@ import $ from 'jquery';
 
 export default Route.extend({
   model() {
-    return Ember.$.getJSON("http://localhost:3000/activity-feed");
+    return Ember.$.getJSON("http://localhost:8080/api/activity-feeds");
   }
 });
