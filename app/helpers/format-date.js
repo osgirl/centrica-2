9c2059a14512d4d2f77203d1fade0a3128635e9b
Andrew McGregor <andy@txm.net>
2018-02-20 12:06:12 +0000

    cast string to integer

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,6 +1,6 @@
 import { helper } from '@ember/component/helper';
 import moment from 'moment';
 
-export const formatDate = (epoch) => moment(epoch).format('h:mma, ddd Do MMMM YYYY');
+export const formatDate = (epoch) => moment(Number(epoch)).format('h:mma, ddd Do MMMM YYYY');
 
 export default helper(formatDate);
