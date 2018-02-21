053f2957dcb9bb3da072488f0bea785936a1b302
Kayleigh Ridd <kridd@zonedigital.com>
2018-02-21 17:44:28 +0000

    Fixed notification test

--- tests/unit/models/notification-test.js
+++ tests/unit/models/notification-test.js
@@ -1,6 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('notifications', 'Unit | Model | notifications', {
+moduleForModel('notification', 'Unit | Model | notification', {
   // Specify the other units that are required for this test.
   needs: []
 });
