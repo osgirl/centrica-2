c3f4aac08213cf798573236a1b6254bcf495f2dc
Andrew McGregor <andy@txm.net>
2018-02-23 11:45:42 +0000

    relationships

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -9,4 +9,3 @@ export default DS.JSONAPIAdapter.extend({
     }
     */
 });
-
--- app/routes/feed.js
+++ app/routes/feed.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return this.get('store').findAll('notification');
+    return this.store.query('notification', {include: 'notification,notification.customers,notification.employees'})
+    //return this.get('store').findAll('notification');
   }
 });
